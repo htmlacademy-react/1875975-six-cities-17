341522101d43415f779ac29bc8f12b7c33fdeea7
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2025-01-20 20:00:36 +0300

    feat: добавляет axios, thunk и токен

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
-import { loadOffers } from './action';
-import offers from '../mocks/offers';
+// import { loadOffers } from './action';
+import { createAPI } from '../services/api';
+// import offers from '../mocks/offers';
 
-const store = configureStore({reducer});
+export const api = createAPI();
 
-store.dispatch(loadOffers(offers));
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
+
+// store.dispatch(loadOffers(offers));
 
 export default store;
