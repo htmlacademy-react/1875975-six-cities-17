5b02d75d6cacee7c5c9b1cc1344a480438629c0b
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2025-01-28 16:30:17 +0300

    feat: стилизует 404 страницу

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,8 +1,16 @@
+import styles from './not-found.module.css';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 function NotFound() {
   return (
-    <div>Not Found 404</div>
+    <section>
+      <div className={styles.notfound__container}>
+        <img className={styles.notfound__img} src="img/cat.svg" alt="Cat." width="861" height="770" />
+        <Link to={AppRoute.Index} className={styles.notfound__link}>Go to main page</Link>
+      </div>
+    </section>
   );
 }
-//стилизовать позже
 
 export default NotFound;
